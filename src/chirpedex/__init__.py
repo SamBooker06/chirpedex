7d299b8ec8c861327bc8abb7a887a4d0cee32344
Sam Booker <156952138+SamBooker06@users.noreply.github.com>
2026-06-12 22:20:38 +0100

    Updated package to use git tag version

--- src/chirpedex/__init__.py
+++ src/chirpedex/__init__.py
@@ -1,6 +1,10 @@
 """
 Chirpedex: A portable bird identification application.
 """
+import importlib.metadata
 
-__version__ = "0.1.0"
+try:
+    __version__ = importlib.metadata.version(__name__)
 
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
